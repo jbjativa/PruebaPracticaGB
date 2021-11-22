674111d687fff4533a32936c9eafb49c889a991c
Jaime Jativa <jjativav@gmail.com>
2021-11-22 17:49:55 -0500

    Fixing EmployeePayHistory Trying fixing scaffolding views and acontroller by regenerating it, but it generate the same code.

--- Models/EmployeePayHistory.cs
+++ Models/EmployeePayHistory.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -9,7 +11,11 @@ namespace PruebaPracticaGB.Models
 {
     public partial class EmployeePayHistory
     {
+        [Key]
+        [Column(Order = 0)]
         public int BusinessEntityId { get; set; }
+        [Key]
+        [Column(Order = 1)]
         public DateTime RateChangeDate { get; set; }
         public decimal Rate { get; set; }
         public byte PayFrequency { get; set; }
